feeeb5a27f8246d0a302849c15046ac9f8c1d4bf
23407 <23407je@hvhs.school.nz>
2026-05-08 12:30:36 +1200

    almostFinished

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ function helloWorld(){
     }
   )
 }
+function goodbyeWorld(){
+  console.log("Running goodbyeWorld()")
+  firebase.database().ref('/').set(
+    {
+      message: 'Goodbye World!'
+    }
+  )
+}
 function users(){
 firebase.database().ref('Users/Coby').set(
     {
@@ -36,12 +44,17 @@ firebase.database().ref('Users/Coby').set(
 }
 function simpleread() {
   console.log("reading the message");
+  HTML_OUTPUT.innerHTML = "";
   firebase.database().ref('/message').on('value', displayMessage, fb_readError);
   console.log("leaving simpleRead");
 }
 
 function displayMessage(snapshot) {
-  HTML_OUTPUT.innerHTML = snapshot.val();
+  if (snapshot.val() == null) {
+  HTML_OUTPUT.innerHTML = "Data is null";
+  } else {
+  HTML_OUTPUT.innerHTML += snapshot.val();
+  }
 }
 function fb_readError(error) {
 console.log("there was an error reading the message");
@@ -52,7 +65,7 @@ leaderboardTable = {
 Hardcore:{
  Users: {
   Coby: 900,
-  Josh: score,
+  Josh: 800,
   Lukas: 830,
   MrB: 400
 }
@@ -70,24 +83,25 @@ Normal:{
 function leaderboards() {
   firebase.database().ref('/').set(leaderboardTable)
 }
-
-function hardcoreclicker() {
-  firebase.database().ref('Hardcore/Users/Josh').set('value', clickerdata)
-}
-function clickerdata(Joshdata) {
-Joshdata.forEach(sortclicker)
-}
 function fb_readLeaderboard() {
   console.log("Reading leaderboard");
   HTML_OUTPUT.innerHTML = "";
-  firebase.database().ref('Hardcore/Users').orderByValue().on('value', displayLeaderboard, fb_readError)
+  firebase.database().ref('Hardcore/Users').orderByValue().on('value', displayLeaderboard, fb_readError);
 }
 
 function displayLeaderboard(snapshot) {
-  snapshot.forEach(sortData)
+  let hardcoreDataOutput = snapshot.val();
+  console.log(hardcoreDataOutput);
+  if (hardcoreDataOutput == null) {
+    HTML_OUTPUT.innerHTML = "Data is null";
+  } else {
+      snapshot.forEach(sortData)
+      console.log("data sent");
+  }
 }
 function sortData(string){
   let data = string.val();
+  
   console.log(string);
   console.log("the data is: " + data);
   let text = "";
@@ -95,4 +109,29 @@ function sortData(string){
   text += usernames
 }
     HTML_OUTPUT.innerHTML += text + ": " + data + "<br>";
+}
+let GLOBAL_User;
+
+function fb_login() {
+  AuthenticatorListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
+
+function fb_handleLogin(_User) {
+  if (_User) {
+    console.log("user is logged in")
+    HTML_OUTPUT.innerHTML += "<br>" + "User is already logged in" + "<br>";
+    GLOBAL_user = _User;
+  } else {
+    console.log("User has not logged in")
+    loginWithGoogle();
+  }
+}
+
+function loginWithGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_User = result.user;
+    console.log("User has logged in")
+  });
 }
